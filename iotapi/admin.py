839c0db0a52db1f610acb28babf756fe639a44c3
ZodiacFireworks <martin.vuelta@gmail.com>
2016-03-10 21:24:13 -0500

    Internet of Things
    Change log:
      * Added iotdevice and iotrecord objects
    
    Files:
      * modified:   db.sqlite3
      * modified:   iotapi/admin.py
      * modified:   iotapi/migrations/0001_initial.py
      * new file:   iotapi/migrations/0002_iotrecord.py
      * new file:   iotapi/migrations/0003_iotrecord_temperature.py
      * modified:   iotapi/models.py
      * modified:   sysmoon/fields/__pycache__/__init__.cpython-34.pyc
      * modified:   sysmoon/fields/__pycache__/measurements.cpython-34.pyc
    #

--- iotapi/admin.py
+++ iotapi/admin.py
@@ -1,9 +1,66 @@
 from django.contrib import admin
 
 from sysmoon.admin import GeoLocatedMonitoringDeviceBaseAdmin
+from sysmoon.admin import BaseRecordBaseAdmin
+
 from iotapi.models import IOTDevice
+from iotapi.models import IOTRecord
 
 
 @admin.register(IOTDevice)
 class IOTDeviceAdmin(GeoLocatedMonitoringDeviceBaseAdmin):
     model = IOTDevice
+
+    list_display = (
+        "code",
+        "status",
+        "begin_of_service",
+        "end_of_service",
+        "latitude",
+        "longitude",
+        "elevation"
+    )
+
+    radio_fields = {"status": admin.VERTICAL}
+
+    def get_fields(self, request, obj=None):
+        if obj is not None:
+            if obj.status == obj.STATUS_OUT_OF_SERVICE:
+                return ("code", "owner", "status", "registration_date", "begin_of_service", "end_of_service", "position")
+
+            return ("code", "owner", "status", "registration_date", "begin_of_service", "position")
+
+        return ("code", "owner", "status", "begin_of_service", "position")
+
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = ["code", ]
+
+        if obj:
+            readonly_fields = [
+                "code",
+                "begin_of_service",
+                "registration_date",
+            ]
+
+            if obj.status == obj.STATUS_OUT_OF_SERVICE:
+                readonly_fields = [
+                    "code",
+                    "owner",
+                    "registration_date",
+                    "begin_of_service",
+                    "end_of_service",
+                    "position",
+                ]
+
+        return readonly_fields
+
+
+@admin.register(IOTRecord)
+class IOTRecordAdmin(BaseRecordBaseAdmin):
+    model = IOTRecord
+
+    list_display = (
+        "device",
+        "log_date",
+        "temperature"
+    )
--- iotapi/models.py
+++ iotapi/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
+
 from sysmoon.models.devices import GeoLocatedMonitoringDevice
+from sysmoon.models.records import BaseRecord
+from sysmoon.fields.measurements import MeasurementField
 
 
 # Create your models here.
 class IOTDevice(GeoLocatedMonitoringDevice):
     # TODO: Define fields here
+    owner = models.ForeignKey(
+        User,
+        default=None,
+        on_delete=models.PROTECT,
+        related_name="devices",
+        related_query_name="owner",
+        verbose_name=_("owner"),
+    )
 
     class Meta(object):
         verbose_name = "IoT Device"
@@ -13,3 +26,28 @@ class IOTDevice(GeoLocatedMonitoringDevice):
 
     def save(self):
         return super(IOTDevice, self).save()
+
+
+class IOTRecord(BaseRecord):
+    device = models.ForeignKey(
+        IOTDevice,
+        default=None,
+        on_delete=models.PROTECT,
+        related_name="records",
+        related_query_name="devices",
+        verbose_name=_("device"),
+    )
+
+    temperature = MeasurementField(
+        _("Temperature"),
+        default=-9999.9999,
+        null=True
+    )
+
+    class Meta(object):
+        verbose_name = "IoT Record"
+        verbose_name_plural = "IoT Records"
+        abstract = False
+
+    def save(self):
+        return super(IOTRecord, self).save()
